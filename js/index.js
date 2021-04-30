bae9f219392a8dc3e252b0fcccc451a0f36d3c9f
Sam <sam@posonas.com>
2021-04-30 09:48:30 +0200

    Edit list

--- js/index.js
+++ js/index.js
@@ -5,13 +5,8 @@ index = {
             ulCategories.innerHTML = '';
             
             categories.forEach(category => {
-                let categoryHeader = document.createElement('div');
-                categoryHeader.classList.add('divCategoryHeader');
-                categoryHeader.innerHTML = `
-                    <h2>${category.category}</h2>
-                `;
+                let categoryHeader = index.create.categoryHeader(category);
                 ulCategories.appendChild(categoryHeader);
-
                 ulCategories.appendChild(index.create.shortcutsList(category.shortcuts));
             })
 
@@ -21,39 +16,7 @@ index = {
             let programList = Id('ulPrograms');
 
             programs.forEach(program => {
-                let programLi = document.createElement('li');
-                programLi.innerHTML = `
-                    <p>${program.program}</p>
-                    <input type="text" style="display:none;">
-                `;
-
-                programLi.params = program;
-                programLi.onclick = function(evt) {
-                    index.set.shortcutCategories(this.params.categories);
-                };
-
-                programLi.ondblclick = function(evt) {
-                    let inputBox = this.querySelector('input');
-                    let programName = this.querySelector('p');
-
-                    inputBox.style.display = "block";
-                    programName.style.display = "none";
-
-                    let programNameText = this.querySelector('p').innerHTML;
-                    inputBox.value = programNameText;
-                    inputBox.focus();
-                }
-
-                programLi.querySelector('input').onblur = programLi.querySelector('input').onchange = function(evt) {
-                    debugger;
-                    let inputBox = this;
-                    let programName = inputBox.parentElement.querySelector('p');
-
-                    programName.innerHTML = inputBox.value;
-
-                    inputBox.style.display = "none";
-                    programName.style.display = "block";
-                }
+                let programLi = index.create.programListItem(program);
 
                 programList.appendChild(programLi);
             });
@@ -94,6 +57,48 @@ index = {
             });
 
             return ulShortcuts;
+        },
+        categoryHeader: function(category) {
+            let categoryHeader = document.createElement('div');
+            categoryHeader.classList.add('divCategoryHeader');
+            categoryHeader.innerHTML = `
+                <h2>${category.category}</h2>
+                <input type="text" style="display:none;">
+            `;
+
+            categoryHeader.ondblclick = index.events.editCategoryName;
+
+            categoryHeader.querySelector('input').onblur = categoryHeader.querySelector('input').onchange = index.events.saveCategoryName;
+            categoryHeader.querySelector('input').onkeyup = function(evt) {
+                if (evt.keyCode === 13) {
+                    index.events.saveCategoryName(evt);
+                }
+            }
+
+            return categoryHeader;
+        },
+        programListItem: function(program) {
+            let programLi = document.createElement('li');
+            programLi.innerHTML = `
+                <p>${program.program}</p>
+                <input type="text" style="display:none;">
+            `;
+
+            programLi.params = program;
+            programLi.onclick = function(evt) {
+                index.set.shortcutCategories(this.params.categories);
+            };
+
+            programLi.ondblclick = index.events.editProgramName;
+
+            programLi.querySelector('input').onblur = programLi.querySelector('input').onchange = index.events.saveProgramName;
+            programLi.querySelector('input').onkeyup = function(evt) {
+                if (evt.keyCode === 13) {
+                    index.events.saveProgramName(evt);
+                }
+            }
+
+            return programLi
         }
     },
     data: {
@@ -162,14 +167,113 @@ index = {
             },
         ],
     },
+    events: {
+        editProgramName: function(evt) {
+            let inputBox = this.querySelector('input');
+            let programName = this.querySelector('p');
+
+            inputBox.style.display = "block";
+            programName.style.display = "none";
+
+            let programNameText = this.querySelector('p').innerHTML;
+            inputBox.value = programNameText;
+            inputBox.focus();
+        },
+        saveProgramName: function(evt) {
+            let inputBox = this;
+            let programName = inputBox.parentElement.querySelector('p');
+
+            programName.innerHTML = inputBox.value;
+
+            inputBox.style.display = "none";
+            programName.style.display = "block";
+        },
+        editCategoryName: function(evt) {
+            let inputBox = this.querySelector('input');
+            let programName = this.querySelector('h2');
+
+            inputBox.style.display = "block";
+            programName.style.display = "none";
+
+            let programNameText = this.querySelector('h2').innerHTML;
+            inputBox.value = programNameText;
+            inputBox.focus();
+        },
+        saveCategoryName: function(evt) {
+            let inputBox = this;
+            let programName = inputBox.parentElement.querySelector('h2');
+
+            programName.innerHTML = inputBox.value;
+
+            inputBox.style.display = "none";
+            programName.style.display = "block";
+        },
+        addNewProgram: function(evt) {
+            let addButton = this;
+            let newProgramObj = {
+                program: "New program",
+                categories: [
+                    {
+                        category: "New category",
+                        shortcuts: []
+                    }
+                ]
+            }
+
+            let newProgramLi = index.create.programListItem(newProgramObj);
+            Id('ulPrograms').appendChild(newProgramLi);
+
+            newProgramLi.click();
+        },
+        addNewGame: function(evt) {
+            let addButton = this;
+            let newGameObj = {
+                program: "New game",
+                categories: [
+                    {
+                        category: "New category",
+                        shortcuts: []
+                    }
+                ]
+            }
+
+            let newGameLi = index.create.programListItem(newGameObj);
+            Id('ulGames').appendChild(newGameLi);
+
+            newGameLi.click();
+        }
+    },
     format: {
         html: function(htlmString) {
 
         }
     }, 
+    procedure: function(url) {
+        fetch(url)
+        .then(
+          function(response) {
+            if (response.status !== 200) {
+              console.log('Looks like there was a problem. Status Code: ' +
+                response.status);
+              return;
+            }
+      
+            // Examine the text in the response
+            response.json().then(function(data) {
+              console.log(data);
+            });
+          }
+        )
+        .catch(function(err) {
+          console.log('Fetch Error :-S', err);
+        });
+    },
     init: function() {
         index.set.shortcutCategories(index.data.programs[0].categories);
         index.set.programList(index.data.programs);
+
+        Id('btnAddProgram').onclick = index.events.addNewProgram;
+        Id('btnAddGame').onclick = index.events.addNewGame;
     }
 }
 
